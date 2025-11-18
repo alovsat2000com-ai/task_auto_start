c20701c1bc6bd8fc5012b7ec6efdddf0e537b2a8
Alik <alovsat.2000.com@gmail.com>
2025-11-18 22:14:43 +0300

    added git address to install.ps1

--- install.ps1
+++ install.ps1
@@ -6,12 +6,12 @@ if (!(Test-Path $Target)) {
 }
 
 # Скачивание BAT-файлов из GitHub RAW
-Invoke-WebRequest "https://raw.githubusercontent.com/USER/REPO/main/Cleaner_v6.bat" -OutFile "$Target\Cleaner_v6.bat"
-Invoke-WebRequest "https://raw.githubusercontent.com/USER/REPO/main/RDP_Cleaner2.bat" -OutFile "$Target\RDP_Cleaner2.bat"
+Invoke-WebRequest "https://raw.githubusercontent.com/alovsat2000.com-ai/task_auto_start/main/Cleaner_v6.bat" -OutFile "$Target\Cleaner_v6.bat"
+Invoke-WebRequest "https://raw.githubusercontent.com/alovsat2000.com-ai/task_auto_start/main/RDP_Cleaner2.bat" -OutFile "$Target\RDP_Cleaner2.bat"
 
 # Скачивание XML расписаний
-Invoke-WebRequest "https://raw.githubusercontent.com/USER/REPO/main/Clear_time_startup_v2.xml" -OutFile "$Target\Clear_time_startup_v2.xml"
-Invoke-WebRequest "https://raw.githubusercontent.com/USER/REPO/main/RDP_Cleaner2.xml" -OutFile "$Target\RDP_Cleaner2.xml"
+Invoke-WebRequest "https://raw.githubusercontent.com/alovsat2000.com-ai/task_auto_start/main/Clear_time_startup_v2.xml" -OutFile "$Target\Clear_time_startup_v2.xml"
+Invoke-WebRequest "https://raw.githubusercontent.com/alovsat2000.com-ai/task_auto_start/main/RDP_Cleaner2.xml" -OutFile "$Target\RDP_Cleaner2.xml"
 
 # Удаляем старые задания
 schtasks /delete /tn "Cleare_time_startup" /f > $null 2>&1
