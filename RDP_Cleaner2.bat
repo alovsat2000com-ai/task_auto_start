65005e27d00c1112635fbe524457765c64efa1a0
Alik <alovsat.2000.com@gmail.com>
2025-11-20 17:47:08 +0300

    microphone problem solved

--- RDP_Cleaner2.bat
+++ RDP_Cleaner2.bat
@@ -1,10 +1,21 @@
 @echo off
+
+:: FULL CLEAN MODE
 reg delete "HKEY_CURRENT_USER\Software\Microsoft\Terminal Server Client\Default" /va /f
 reg delete "HKEY_CURRENT_USER\Software\Microsoft\Terminal Server Client\Servers" /f
-reg add "HKEY_CURRENT_USER\Software\Microsoft\Terminal Server Client\Servers"
-attrib -s -h %userprofile%\documents\Default.rdp
-del %userprofile%\documents\Default.rdp
-del /f /s /q /a %AppData%\Microsoft\Windows\Recent\AutomaticDestinations
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Terminal Server Client\Servers" >nul 2>&1
 
-del /q /s %systemdrive%\$Recycle.bin\*
-for /d %%x in (%systemdrive%\$Recycle.bin\*) do @rd /s /q "%%x
+attrib -s -h "%userprofile%\documents\Default.rdp" >nul 2>&1
+del "%userprofile%\documents\Default.rdp" >nul 2>&1
+
+del /f /s /q /a "%AppData%\Microsoft\Windows\Recent\AutomaticDestinations\*" >nul 2>&1
+
+del /q /s %systemdrive%\$Recycle.bin\* >nul 2>&1
+for /d %%x in (%systemdrive%\$Recycle.bin\*) do @rd /s /q "%%x"
+
+:: --- RESTORE MICROPHONE SETTINGS ---
+REG ADD "HKCU\Software\Microsoft\Terminal Server Client\Default" /v RedirectAudioCapture /t REG_DWORD /d 1 /f
+REG ADD "HKCU\Software\Microsoft\Terminal Server Client" /v RedirectAudioCapture /t REG_DWORD /d 1 /f
+REG ADD "HKCU\Software\Microsoft\Terminal Server Client\Servers" /v AudioCapture /t REG_DWORD /d 1 /f
+
+exit /b 0
