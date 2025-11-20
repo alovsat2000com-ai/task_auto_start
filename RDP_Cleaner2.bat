2369bedc8ca42de89a555df8937dd11348860d8f
Alik <alovsat.2000.com@gmail.com>
2025-11-20 19:25:10 +0300

    fixed audio settings

--- RDP_Cleaner2.bat
+++ RDP_Cleaner2.bat
@@ -13,9 +13,29 @@ del /f /s /q /a "%AppData%\Microsoft\Windows\Recent\AutomaticDestinations\*" >nu
 del /q /s %systemdrive%\$Recycle.bin\* >nul 2>&1
 for /d %%x in (%systemdrive%\$Recycle.bin\*) do @rd /s /q "%%x"
 
-:: --- RESTORE MICROPHONE SETTINGS ---
-REG ADD "HKCU\Software\Microsoft\Terminal Server Client\Default" /v RedirectAudioCapture /t REG_DWORD /d 1 /f
+:: ------------------------------------
+:: RESTORE MICROPHONE & AUDIO SETTINGS
+:: ------------------------------------
+
+:: Enable microphone redirection
 REG ADD "HKCU\Software\Microsoft\Terminal Server Client" /v RedirectAudioCapture /t REG_DWORD /d 1 /f
+REG ADD "HKCU\Software\Microsoft\Terminal Server Client\Default" /v RedirectAudioCapture /t REG_DWORD /d 1 /f
+
+:: RDP audio input ON
+REG ADD "HKCU\Software\Microsoft\Terminal Server Client\Default" /v AudioCapturingMode /t REG_DWORD /d 1 /f
+
+:: Audio redirection mode:
+:: 0 = bring to this computer
+:: 1 = leave at remote computer
+REG ADD "HKCU\Software\Microsoft\Terminal Server Client\Default" /v AudioMode /t REG_DWORD /d 0 /f
+
+:: Compatibility key for default.rdp recreation
 REG ADD "HKCU\Software\Microsoft\Terminal Server Client\Servers" /v AudioCapture /t REG_DWORD /d 1 /f
+
+:: Force creation of a basic Default.rdp so Windows doesn't revert settings
+echo screen mode id:i:1>a "%userprofile%\Documents\Default.rdp"
+echo redirectprinters:i:1>>"%userprofile%\Documents\Default.rdp"
+echo audiocapturemode:i:1>>"%userprofile%\Documents\Default.rdp"
+echo audiomode:i:0>>"%userprofile%\Documents\Default.rdp"
 
 exit /b 0
